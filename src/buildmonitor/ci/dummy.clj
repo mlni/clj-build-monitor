3f1946c5a314d9bc64124def577716b657663a2e
Matti Jagula <matti.jagula@gmail.com>
2015-12-09 09:00:18 +0200

    Added a dummy build with random state to test transitions

--- src/buildmonitor/ci/dummy.clj
+++ src/buildmonitor/ci/dummy.clj
@@ -6,9 +6,13 @@
     (take n (map (fn [i status]
                    {:id i :status status :number i}) (iterate inc 1) statuses))))
 
+(defn- rand-state []
+  (rand-nth [:success :failed :running :canceled]))
+
 (defn fetch-service [_]
   (log/info "Returning dummy data")
   [{:id 1 :name "Build-1" :number 42 :status :success :seconds-since 12 :history (gen-history 5)}
    {:id 2 :name "Build-2" :number 10001 :status :failed :seconds-since 12000 :history []}
    {:id 3 :name "Build-3" :number 1 :status :canceled :seconds-since 680 :history (gen-history 10)}
-   {:id 4 :name "Build-4" :number 13 :status :running :seconds-since 600000 :history (gen-history 10)}])
+   {:id 4 :name "Build-4" :number 13 :status :running :seconds-since 600000 :history (gen-history 10)}
+   {:id 5 :name "Build-5" :number 1234 :status (rand-state) :seconds-since 300 :history (gen-history 10)}])
